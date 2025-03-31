7603cf553bcaa37f7ea9c9c1da930a1a416079dd
Luka <lukhanyo95@gmail.com>
2025-03-31 14:46:36 +0200

    Database initial setup

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-// Create needed constants
 const list = document.querySelector('ul');
 const titleInput = document.querySelector('#title');
 const bodyInput = document.querySelector('#body');
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('form button');
+
+let db;
+const openRequest = window.indexedDB.open("notes_db", 1);
+
+openRequest.addEventListener("error", () =>
+    console.error("Database failed to open"),
+  );
+
+  openRequest.addEventListener("success", () => {
+    console.log("Database opened successfully");
+  
+    db = openRequest.result;
+
+    displayData();
+  });
+
+  openRequest.addEventListener("upgradeneeded", (e) => {
+ 
+    db = e.target.result;
+  
+    const objectStore = db.createObjectStore("notes_os", {
+      keyPath: "id",
+      autoIncrement: true,
+    });
+  
+    objectStore.createIndex("title", "title", { unique: false });
+    objectStore.createIndex("body", "body", { unique: false });
+  
+    console.log("Database setup complete");
+  });
